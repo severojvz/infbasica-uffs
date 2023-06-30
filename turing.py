1bee625607106124af054ee98be61da8f640e913
Eduardo P Zancanaro <eduardo.zancanaro@estudante.uffs.edu.br>
2023-06-30 15:07:59 -0300

    Trabalho

--- turing.py
+++ turing.py
@@ -1,82 +1,19 @@
-def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
-    return mensagem
+ohn McCarthy (Boston, 4 de setembro de 1927 — Stanford, Califórnia, 23 de outubro de 2011[2]) foi um cientista da computação estadunidense.
 
+Conhecido pelos estudos no campo da inteligência artificial e por ser o criador da linguagem de programação Lisp. Recebeu o Prêmio Turing de 1972 e a Medalha Nacional de Ciências dos Estados Unidos de 1991.[3]
+Publicações selecionadas
 
-def doutorado():
-    mensagem = ""
-    return mensagem
-
-
-def contribuicoes():
-    mensagem = ""
-    return mensagem
-
-
-def artigos():
-    mensagem = ""
-    return mensagem
-
-
-def citacoes():
-    mensagem = ""
-    return mensagem
-
-
-def sair():
-    mensagem = "\nObrigado pela leitura!"
-    return mensagem
-
-
-def erro():
-    mensagem = "\nOpção inválida!"
-    return mensagem
-
-
-print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
-
-continuar = True
-while continuar == True:
-
-    opcao = int(
-        input(
-"""
-\nDigite o número correspondente ao menu que você deseja acessar:
-1 - Resumo
-2 - Doutorado
-3 - Contribuições
-4 - Principais Artigos
-5 - Citações
-6 - Sair\n
-"""
-        )
-    )
-
-    if opcao == 1:
-        print("1 - Resumo")
-        mensagem = resumo()
-
-    elif opcao == 2:
-        print("2 - Doutorado")
-        mensagem = doutorado()
-
-    elif opcao == 3:
-        print("3 - Contribuições")
-        mensagem = contribuicoes()
-
-    elif opcao == 4:
-        print("4 - Principais Artigos")
-        mensagem = artigos()
-
-    elif opcao == 5:
-        print("5 - Citações")
-        mensagem = citacoes()
-
-    elif opcao == 6:
-        mensagem = sair()
-        continuar = False
-
-    else:
-        mensagem = erro()
-
-    print(mensagem)
+    McCarthy, J. 1959. Programs with Common Sense. In Proceedings of the Teddington Conference on the Mechanization of Thought Processes, 756-91. London: Her Majesty's Stationery Office.
+    McCarthy, J. 1960. Recursive functions of symbolic expressions and their computation by machine. Communications of the ACM 3(4):184-195.
+    McCarthy, J. 1963a A basis for a mathematical theory of computation. In Computer Programming and formal systems. North-Holland.
+    McCarthy, J. 1963b. Situations, actions, and causal laws. Technical report, Stanford University.
+    McCarthy, J., and Hayes, P. J. 1969. Some philosophical problems from the standpoint of artificial intelligence. In Meltzer, B., and Michie, D., eds., Machine Intelligence 4. Edinburgh: Edinburgh University Press. 463-502.
+    McCarthy, J. 1977. Epistemological problems of artificial intelligence. In IJCAI, 1038-1044.
+    McCarthy, J. 1980. Circumscription: A form of non-monotonic reasoning. Artificial Intelligence 13(1-2):23-79.
+    McCarthy, J. 1986. Applications of circumscription to common sense reasoning. Artificial Intelligence 28(1):89-116.
+    McCarthy, J. 1990. Generality in artificial intelligence. In Lifschitz, V., ed., Formalizing Common Sense. Ablex. 226-236.
+    McCarthy, J. 1993. Notes on formalizing context. In IJCAI, 555-562.
+    McCarthy, J., and Buvac, S. 1997. Formalizing context: Expanded notes. In Aliseda, A.; van Glabbeek, R.; and Westerstahl, D., eds., Computing Natural Language. Stanford University. Also available as Stanford Technical Note STAN-CS-TN-94-13.
+    McCarthy, J. 1998. Elaboration tolerance. In Working Papers of the Fourth International Symposium on Logical formalizations of Commonsense Reasoning, Commonsense-1998.
+    Costello, T., and McCarthy, J. 1999. Useful counterfactuals. Electronic Transactions on Artificial Intelligence 3(A):51-76
+    McCarthy, J. 2002. Actions and other events in situation calculus. In Fensel, D.; Giunchiglia, F.; McGuinness, D.; and Williams, M., eds., Proceedings of KR-2002, 615-628.
